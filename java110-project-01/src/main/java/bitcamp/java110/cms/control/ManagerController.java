f1bbe36a2b6255709808d5906b287709f3df20ac
Yun Gun Hee <bear930920@gmail.com>
2018-09-05 16:33:40 +0900

    매니저 목록 처리 별도 클래스로 분리

--- java110-project-01/src/main/java/bitcamp/java110/cms/control/ManagerController.java
+++ java110-project-01/src/main/java/bitcamp/java110/cms/control/ManagerController.java
@@ -1,30 +1,13 @@
 package bitcamp.java110.cms.control;
 
 import java.util.Scanner;
-import bitcamp.java110.cms.domain.Member;
+
+import bitcamp.java110.cms.dao.ManagerList;
+import bitcamp.java110.cms.domain.Manager;
 
 public class ManagerController {
-    static Manager[] managers=new Manager[100];
-    static int managerIndex = 0;
+
     public static Scanner keyIn;
-    
-    static class Manager extends Member{
-        protected String tel;
-        protected String position;
-        public String getTel() {
-            return tel;
-        }
-        public void setTel(String tel) {
-            this.tel = tel;
-        }
-        public String getPosition() {
-            return position;
-        }
-        public void setPosition(String position) {
-            this.position = position;
-        }
-        
-    }
 
     public static void serviceManagerMenu() {
         while(true) {
@@ -48,9 +31,10 @@ public class ManagerController {
     
     private static void printManagers() {
         int count=0;
-        for (Manager s : managers) {
-            if(count++ == managerIndex)
+        for (int i=0;i<ManagerList.size();i++) {
+            if(count++ == ManagerList.size())
                 break;
+            Manager s= ManagerList.get(i);
             System.out.printf("%s, %s, %s %s %b %s\n", 
                     s.getName(), 
                     s.getEmail(), 
@@ -78,12 +62,7 @@ public class ManagerController {
             System.out.print("position? ");
             s.setPosition(keyIn.nextLine());
             
-            if(managerIndex==managers.length) {
-                increaseStorage();
-            }
-            
-            managers[managerIndex++] = s;
-            
+            ManagerList.add(s);
             
             System.out.print("계속 하시겠습니까?(Y/n) ");
             String answer = keyIn.nextLine();
@@ -91,27 +70,15 @@ public class ManagerController {
                 break;
         }
     }
-    private static void increaseStorage() {
-        Manager[] newList = new Manager[managers.length+3];
-        
-        for(int i=0; i<managers.length;i++) {
-            managers[i]=newList[i];
-        }
-        managers=newList;
-    }
     
     private static void deleteManager() {
         System.out.print("삭제할 번호?");
         int no= Integer.parseInt(keyIn.nextLine());
-        
-        if(no<0 || no >= managerIndex) {
+        if(no<0 || no >= ManagerList.size()) {
             System.out.println("무효한 번호입니다.");
             return;
         }
-        for (int i = no; i < managerIndex - 1; i++) {
-            managers[i] = managers[i + 1];
-        }
-        managerIndex--;
+        ManagerList.remove(no);
         
         System.out.println("삭제하였습니다.");
         
@@ -119,16 +86,16 @@ public class ManagerController {
     private static void detailManager() {
         System.out.print("조회할 번호?");
         int no= Integer.parseInt(keyIn.nextLine());
-        
-        if(no<0 || no >= managerIndex) {
+        if(no<0 || no >= ManagerList.size()) {
             System.out.println("무효한 번호입니다.");
             return;
         }
-        System.out.printf("이름: %s\n",managers[no].getName());
-        System.out.printf("이메일: %s\n",managers[no].getEmail());
-        System.out.printf("암호: %s\n",managers[no].getPassword());
-        System.out.printf("전화: %s\n",managers[no].getTel());
-        System.out.printf("Position: %b\n",managers[no].getPosition());
+        Manager s=ManagerList.get(no);
+        System.out.printf("이름: %s\n",s.getName());
+        System.out.printf("이메일: %s\n",s.getEmail());
+        System.out.printf("암호: %s\n",s.getPassword());
+        System.out.printf("전화: %s\n",s.getTel());
+        System.out.printf("Position: %b\n",s.getPosition());
         
     }
 }
