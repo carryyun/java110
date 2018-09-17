da1d061e19f4d7252cc135780dec672056621cc0
Yun Gun Hee <bear930920@gmail.com>
2018-09-17 17:28:18 +0900

    Stateless 적용

--- java110-project-01/src/main/java/bitcamp/java110/cms/server/ServerApp.java
+++ java110-project-01/src/main/java/bitcamp/java110/cms/server/ServerApp.java
@@ -51,8 +51,10 @@ public class ServerApp {
         // 클라이언트 연결을 기다리는 서버 소켓 준비
         ServerSocket serversocket = new ServerSocket(8888);
         System.out.println("서버 실행중...");
+        
         while(true) {
             Socket socket = serversocket.accept();
+            System.out.println("클라이언트가 연결되었음.");
             RequestWorker worker = new RequestWorker(socket);
             new Thread(worker).start();
 
@@ -70,7 +72,7 @@ public class ServerApp {
         public RequestWorker(Socket socket) {
             this.socket=socket;
         }
-        
+
         @Override
         public void run() {
             // 이 메서드에서 "main" 스레드에서 분리하여 독립적으로 수행할 코드를 둔다.
@@ -80,48 +82,39 @@ public class ServerApp {
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     ){
 
-                System.out.println(in.readLine());
+                String requestLine = in.readLine();
+                System.out.println("클라이언트 요청 받았음.");
+                
+                // 요청 객체 준비
+                Request request=new Request(requestLine);
 
-                out.println("OK: 윤건희"); out.flush();
+                // 응답 객체 준비
+                Response response = new Response(out);
 
-                while (true) {
-                    String requestLine = in.readLine();
-                    if (requestLine.equals("EXIT")){
-                        out.println("good bye!");
-                        out.println();
-                        out.flush();
-                        break;
-                    }
-
-                    // 요청 객체 준비
-                    Request request=new Request(requestLine);
-
-                    // 응답 객체 준비
-                    Response response = new Response(out);
-
-                    RequestMappingHandler mapping = requestHandlerMap.getMapping(request.getAppPath());
-                    if (mapping == null) {
-                        out.println("해당 요청을 처리할 수 없습니다.");
-                        out.println();
-                        out.flush();
-                        continue;
-                    }
-                    try {
-
-                        // 요청 핸들러 호출
-                        mapping.getMethod().invoke(mapping.getInstance(), request, response);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        out.println("요청 처리중 오류가 발생했습니다.");
-                        out.println();
-                        out.flush();
-                    }
+                RequestMappingHandler mapping = requestHandlerMap.getMapping(request.getAppPath());
+                if (mapping == null) {
+                    out.println("해당 요청을 처리할 수 없습니다.");
                     out.println();
                     out.flush();
+                    return;
                 }
+                try {
+
+                    // 요청 핸들러 호출
+                    mapping.getMethod().invoke(mapping.getInstance(), request, response);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    out.println("요청 처리중 오류가 발생했습니다.");
+                }
+                out.println();
+                out.flush();
+                
 
             }catch(Exception e) {
                 System.out.println(e.getMessage());
+            }finally {
+                System.out.println("클라이언트에게 응답했음.");
+                System.out.println("클라이언트와 연결을 끊음.");
             }
         } // run()
     } //RequestWorker
