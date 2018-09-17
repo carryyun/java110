3923c996e9fcd0ec81fd919ffa2b3f440b5b86e3
Yun Gun Hee <bear930920@gmail.com>
2018-09-17 13:07:15 +0900

    멀티 스레드 적용

--- java110-project-01/src/main/java/bitcamp/java110/cms/server/ServerApp.java
+++ java110-project-01/src/main/java/bitcamp/java110/cms/server/ServerApp.java
@@ -16,19 +16,19 @@ public class ServerApp {
 
     ClassPathXmlApplicationContext iocContainer;
     RequestMappingHandlerMapping requestHandlerMap;
-    
+
     ServerApp(){
-        
+
         createIoCContainer();
         logBeansOfContainer();
         processRequestMappingAnnotation();
-        
+
     }
-    
+
     private void createIoCContainer() {
         iocContainer = new ClassPathXmlApplicationContext("bitcamp/java110/cms/conf/application-context.xml");
     }
-    
+
     private void logBeansOfContainer() {
         System.out.println("================================================");
         String[] nameList = iocContainer.getBeanDefinitionNames();
@@ -45,25 +45,45 @@ public class ServerApp {
             requestHandlerMap.addMapping(obj);
         }
     }
-    
-    
-    public void service() throws Exception{
-     // 클라이언트 연결을 기다리는 서버 소켓 준비
-        ServerSocket serversocket = new ServerSocket(8888);
 
+
+    public void service() throws Exception{
+        // 클라이언트 연결을 기다리는 서버 소켓 준비
+        ServerSocket serversocket = new ServerSocket(8888);
+        System.out.println("서버 실행중...");
         while(true) {
-            System.out.println("접속 대기중...");
+            Socket socket = serversocket.accept();
+            RequestWorker worker = new RequestWorker(socket);
+            new Thread(worker).start();
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ServerApp serverApp = new ServerApp();
+        serverApp.service();
+
+    }
+
+    class RequestWorker implements Runnable{
+        Socket socket;
+        public RequestWorker(Socket socket) {
+            this.socket=socket;
+        }
+        
+        @Override
+        public void run() {
+            // 이 메서드에서 "main" 스레드에서 분리하여 독립적으로 수행할 코드를 둔다.
             try (
-                    
-                    Socket socket = serversocket.accept();
+                    Socket socket = this.socket; // 밖에서 선언한 socket을 AutoClose 하기위함
                     PrintWriter out = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()));
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     ){
-                
+
                 System.out.println(in.readLine());
-                
+
                 out.println("OK: 윤건희"); out.flush();
-                
+
                 while (true) {
                     String requestLine = in.readLine();
                     if (requestLine.equals("EXIT")){
@@ -72,13 +92,13 @@ public class ServerApp {
                         out.flush();
                         break;
                     }
-                    
+
                     // 요청 객체 준비
                     Request request=new Request(requestLine);
-                    
+
                     // 응답 객체 준비
                     Response response = new Response(out);
-                    
+
                     RequestMappingHandler mapping = requestHandlerMap.getMapping(request.getAppPath());
                     if (mapping == null) {
                         out.println("해당 요청을 처리할 수 없습니다.");
@@ -87,7 +107,7 @@ public class ServerApp {
                         continue;
                     }
                     try {
-                        
+
                         // 요청 핸들러 호출
                         mapping.getMethod().invoke(mapping.getInstance(), request, response);
                     } catch (Exception e) {
@@ -100,17 +120,11 @@ public class ServerApp {
                     out.flush();
                 }
 
-
             }catch(Exception e) {
-
+                System.out.println(e.getMessage());
             }
-        }
-    }
+        } // run()
+    } //RequestWorker
 
-    public static void main(String[] args) throws Exception {
-        ServerApp serverApp = new ServerApp();
-        serverApp.service();
-        
-    }
-}
+}//ServerApp
 
